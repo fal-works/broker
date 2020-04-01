67ec299d8d3a865bda3a7003aeaf9e6da6bb4739
FAL <falworks.contact@gmail.com>
2020-04-01 21:56:04 +0900

    refactor

--- test/integration/ActorAosoaBuilder.hx
+++ test/integration/ActorAosoaBuilder.hx
@@ -2,23 +2,31 @@ package integration;
 
 import banker.common.MathTools.minInt;
 
+typedef ActorAosoaConstructor = (
+	chunkCapacity: Int,
+	chunkCount: Int,
+	batch: h2d.SpriteBatch,
+	spriteFactory: () -> h2d.SpriteBatch.BatchElement,
+	fireCallback: FireCallback
+) -> Void;
+
+typedef ActorAosoaConstructible = haxe.Constraints.Constructible<ActorAosoaConstructor>;
+
 class ActorAosoaBuilder {
 	public static var defaultChunkCapacity = 64;
 
-	/**
-		Creates an `ActorAosoa` instance.
-	**/
-	public static function create(
+	@:generic
+	public static function create<T: ActorAosoaConstructible>(
 		maxEntityCount: Int,
 		batch: h2d.SpriteBatch,
 		fireCallback: FireCallback
-	): ActorAosoa {
+	): T {
 		final chunkCapacity = minInt(defaultChunkCapacity, maxEntityCount);
 		final chunkCount = Math.ceil(maxEntityCount / chunkCapacity);
 
 		final tile = batch.tile;
 		final spriteFactory = () -> new h2d.SpriteBatch.BatchElement(tile);
-		final aosoa = new ActorAosoa(
+		final aosoa = new T(
 			chunkCapacity,
 			chunkCount,
 			batch,
