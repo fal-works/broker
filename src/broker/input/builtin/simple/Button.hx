64a0e7b34d493d884c2e176564f7beef6e499015
FAL <falworks.contact@gmail.com>
2020-04-03 18:37:24 +0900

    input: add comment

--- src/broker/input/builtin/simple/Button.hx
+++ src/broker/input/builtin/simple/Button.hx
@@ -2,15 +2,30 @@ package broker.input.builtin.simple;
 
 /**
 	A simple set of virtual buttons consisting of
-	A, B, X, Y and 4 direction buttons.
+	A, B, X, Y and cross direction buttons.
 **/
 enum abstract Button(Int) {
+	/** Virtual A button. **/
 	var A;
+
+	/** Virtual A button. **/
 	var B;
+
+	/** Virtual X button. **/
 	var X;
+
+	/** Virtual Y button. **/
 	var Y;
+
+	/** Virtual left direction button. **/
 	var LEFT;
+
+	/** Virtual up direction button. **/
 	var UP;
+
+	/** Virtual right direction button. **/
 	var RIGHT;
+
+	/** Virtual down direction button. **/
 	var DOWN;
 }
