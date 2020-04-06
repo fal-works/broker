5803c239e9534bc37d41cf981c4a5ba3fe28661e
FAL <falworks.contact@gmail.com>
2020-04-06 22:05:15 +0900

    input: GenericButtonStatusMap.createUpdater()

--- src/broker/input/interfaces/GenericButtonStatusMap.hx
+++ src/broker/input/interfaces/GenericButtonStatusMap.hx
@@ -5,4 +5,12 @@ import banker.finite.interfaces.FiniteKeysMap;
 /**
 	Mapping from virtual buttons (of any enum abstract type) to their corresponding status.
 **/
-interface GenericButtonStatusMap<T> extends FiniteKeysMap<T, ButtonStatus> {}
+interface GenericButtonStatusMap<T> extends FiniteKeysMap<T, ButtonStatus> {
+	/**
+		Creates a function for updating `this` every frame.
+		@param getButtonChecker Function that returns another function
+		for checking if a given `button` is down.
+		@return Function that updates all button status of `this`.
+	**/
+	function createUpdater(getButtonChecker: (button: T) -> (() -> Bool)): () -> Void;
+}
