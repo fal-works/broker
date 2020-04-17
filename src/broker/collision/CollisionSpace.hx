2ae78492202ca7d2effa7915a066b1aa68e89fa5
FAL <falworks.contact@gmail.com>
2020-04-17 23:09:25 +0900

    collision: add @:remove to CollisionSpace

--- src/broker/collision/CollisionSpace.hx
+++ src/broker/collision/CollisionSpace.hx
@@ -4,4 +4,5 @@ package broker.collision;
 	A static class that represents a 2D space with quadtree space partitioning.
 **/
 @:autoBuild(broker.collision.macro.CollisionSpaceMacro.build())
+@:remove
 interface CollisionSpace {}
