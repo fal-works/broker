d3b672a6cda4146d02c871b87947dfebcaa4f49e
FAL <falworks.contact@gmail.com>
2020-05-02 16:52:37 +0900

    math: add PI constants

--- src/broker/math/Constants.hx
+++ src/broker/math/Constants.hx
@@ -2,7 +2,27 @@ package broker.math;
 
 class Constants {
 	/**
+		π
+	**/
+	public static extern inline final PI: Float = 3.141592653589793;
+
+	/**
 		2π
 	**/
-	public static extern inline final TWO_PI: Float = 6.283185307179586;
+	public static extern inline final TWO_PI: Float = 2 * PI;
+
+	/**
+		-π
+	**/
+	public static extern inline final MINUS_PI: Float = -PI;
+
+	/**
+		2π
+	**/
+	public static extern inline final HALF_PI: Float = PI / 2;
+
+	/**
+		2π
+	**/
+	public static extern inline final MINUS_HALF_PI: Float = MINUS_PI / 2;
 }
