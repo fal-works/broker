e2374d070c0c628ba1152083b890ecd39880b3a6
FAL <falworks.contact@gmail.com>
2020-05-03 20:05:45 +0900

    scene: add comments

--- src/broker/scene/Scene.hx
+++ src/broker/scene/Scene.hx
@@ -4,8 +4,23 @@ package broker.scene;
 	Base class for implementing `broker.scene.interfaces.Scene`.
 **/
 class Scene implements broker.scene.interfaces.Scene {
+	/**
+		Updates `this` scene.
+	**/
 	public function update(): Void {}
+
+	/**
+		Called when `this` scene becomes the top in the scene stack.
+	**/
 	public function activate(): Void {}
+
+	/**
+		Called when `this` scene is no more the top in the scene stack but is not immediately destroyed.
+	**/
 	public function deactivate(): Void {}
+
+	/**
+		Destroyes `this` scene.
+	**/
 	public function destroy(): Void {}
 }
