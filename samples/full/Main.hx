80748344fb082609af12977394ca260da6890e7a
FAL <falworks.contact@gmail.com>
2020-05-03 19:46:10 +0900

    samples/full: use scenes

--- samples/full/Main.hx
+++ samples/full/Main.hx
@@ -1,21 +1,26 @@
 package full;
 
+import broker.scene.SceneStack;
+import full.scenes.PlayScene;
+
 class Main extends hxd.App {
 	static function main()
 		new Main();
 
-	var world: World;
+	var sceneStack: SceneStack;
 
 	override function init() {
 		broker.input.heaps.HeapsKeyTools.initialize();
 		broker.input.heaps.HeapsPadTools.initialize();
+		broker.scene.heaps.Scene.initialize(this);
 		Global.initialize(s2d);
 
-		world = new World(s2d);
+		final initialScene = new PlayScene(s2d);
+		sceneStack = new SceneStack(initialScene, 16).newTag("scene stack");
 	}
 
 	override function update(dt: Float) {
 		Global.update();
-		world.update();
+		sceneStack.update();
 	}
 }
---
+++ samples/full/scenes/PlayScene.hx
@@ -0,0 +1,18 @@
+package full.scenes;
+
+import broker.scene.heaps.Scene;
+import full.World;
+
+class PlayScene extends Scene {
+	final world: World;
+
+	public function new(heapsScene: h2d.Scene) {
+		super(heapsScene);
+		this.world = new World(this.heapsScene);
+	}
+
+	override function update(): Void {
+		super.update();
+		world.update();
+	}
+}
