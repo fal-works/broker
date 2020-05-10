87cbd1eafa5b9e8ed33efee5277b9571f3795a97
FAL <falworks.contact@gmail.com>
2020-05-10 21:24:22 +0900

    scene: let heaps.Layer extend h2d.Object

--- src/broker/scene/heaps/Layer.hx
+++ src/broker/scene/heaps/Layer.hx
@@ -2,10 +2,4 @@ package broker.scene.heaps;
 
 import h2d.Object;
 
-class Layer implements broker.scene.interfaces.Layer {
-	final heapsLayer: Object;
-
-	public function new(?parent: Object) {
-		this.heapsLayer = new Object();
-	}
-}
+class Layer extends h2d.Object implements broker.scene.interfaces.Layer {}
