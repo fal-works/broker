814b24c03906f98d00df839fa5a0ede06eb35a8f
FAL <falworks.contact@gmail.com>
2020-05-14 12:34:14 +0900

    timer: add FadeInTimer

---
+++ src/broker/timer/builtin/FadeInTimer.hx
@@ -0,0 +1,45 @@
+package broker.timer.builtin;
+
+import banker.pool.SafeObjectPool;
+
+class FadeInTimer extends FadeTimerBase {
+	/**
+		Object pool for `FadeInTimer`.
+	**/
+	public static final pool = {
+		final pool = new SafeObjectPool(UInt.one, () -> new FadeInTimer());
+		pool.newTag("FadeInTimer pool");
+		pool;
+	}
+
+	/**
+		Returns a `FadeInTimer` instance that is currently not in use.
+
+		The instance is automatically recycled when completed so that it can be reused again
+		(so `step()` should not be called again after completing).
+
+		@param object The object to change the alpha value.
+		@param duration
+		@return A `FadeInTimer` instance.
+	**/
+	public static function use(
+		object: h2d.Object,
+		duration: UInt
+	): FadeInTimer {
+		final timer = pool.get();
+		timer.reset(object, duration, false);
+		return timer;
+	}
+
+	public function new()
+		super();
+
+	override public function onProgress(progress: Float): Void {
+		this.object.alpha = progress;
+	}
+
+	override public function onComplete(): Void {
+		this.object.alpha = 1.0;
+		pool.put(this);
+	}
+}
