806d97870cca0670d025776c8c30a69e9fca04d8
FAL <falworks.contact@gmail.com>
2020-05-19 00:21:21 +0900

    timer: refactor pools

--- src/broker/timer/builtin/heaps/FadeInTimer.hx
+++ src/broker/timer/builtin/heaps/FadeInTimer.hx
@@ -5,15 +5,9 @@ import banker.pool.SafeObjectPool;
 import broker.timer.Timer;
 import broker.timer.builtin.heaps.ObjectTimer;
 
-final class FadeInTimer extends ObjectTimer<h2d.Object> {
-	/**
-		The object pool to which `this` belongs.
-	**/
-	var pool: Maybe<SafeObjectPool<FadeInTimer>>;
-
+class FadeInTimer extends ObjectTimer<h2d.Object> {
 	public function new() {
 		super();
-		this.pool = Maybe.none();
 	}
 
 	override function onProgress(progress: Float): Void {
@@ -23,26 +17,37 @@ final class FadeInTimer extends ObjectTimer<h2d.Object> {
 	override function onComplete(): Void {
 		super.onComplete();
 		this.object.alpha = 1.0;
-
-		final pool = this.pool;
-		if (pool.isSome()) {
-			pool.unwrap().put(this);
-			this.pool = Maybe.none();
-		}
 	}
 }
 
-class FadeInTimerPool extends SafeObjectPool<FadeInTimer> {
-	public function new(capacity: UInt) {
-		super(capacity, () -> new FadeInTimer());
+final class PooledFadeInTimer extends FadeInTimer {
+	/**
+		The object pool to which `this` belongs.
+	**/
+	var pool: SafeObjectPool<PooledFadeInTimer>;
+
+	public function new(pool: SafeObjectPool<PooledFadeInTimer>) {
+		super();
+		this.pool = pool;
 	}
 
-	override public function get(): FadeInTimer {
+	override function onComplete(): Void {
+		super.onComplete();
+		this.pool.put(this);
+	}
+}
+
+class FadeInTimerPool extends SafeObjectPool<PooledFadeInTimer> {
+	public function new(capacity: UInt) {
+		super(capacity, () -> new PooledFadeInTimer(this));
+	}
+
+	override public function get(): PooledFadeInTimer {
 		throw "Not implemented. Call use() instead of get().";
 	}
 
 	/**
-		Gets a `FadeInTimer` instance that is currently not in use,
+		Gets a `PooledFadeInTimer` instance that is currently not in use,
 		and also resets some variables. Use this method instead of `get()`.
 
 		The instance is automatically recycled when completed so that it can be reused again
@@ -50,14 +55,14 @@ class FadeInTimerPool extends SafeObjectPool<FadeInTimer> {
 
 		@param object The object to change the alpha value.
 		@param duration
-		@return A `FadeInTimer` instance.
+		@return A `PooledFadeInTimer` instance.
 	**/
-	@:access(broker.timer.builtin.heaps.FadeInTimer)
-	public function use(object: h2d.Object, duration: UInt): FadeInTimer {
+	@:access(broker.timer.builtin.heaps.PooledFadeInTimer)
+	public function use(object: h2d.Object, duration: UInt): PooledFadeInTimer {
 		final timer = super.get();
 		TimerExtension.reset(timer, duration);
 		timer.object = object;
-		final pool: SafeObjectPool<FadeInTimer> = this;
+		final pool: SafeObjectPool<PooledFadeInTimer> = this;
 		timer.pool = pool;
 		return timer;
 	}
--- src/broker/timer/builtin/heaps/FadeOutTimer.hx
+++ src/broker/timer/builtin/heaps/FadeOutTimer.hx
@@ -4,16 +4,11 @@ package broker.timer.builtin.heaps;
 import banker.pool.SafeObjectPool;
 import broker.timer.Timer;
 
-final class FadeOutTimer extends ObjectTimer<h2d.Object> {
-	/**
-		The object pool to which `this` belongs.
-	**/
-	var pool: Maybe<SafeObjectPool<FadeOutTimer>>;
-
+class FadeOutTimer extends ObjectTimer<h2d.Object> {
 	/**
 		If `true`, calls `object.remove()` when completing.
 	**/
-	var removeOnComplete: Bool = false;
+	public var removeOnComplete: Bool = false;
 
 	public function new()
 		super();
@@ -31,26 +26,37 @@ final class FadeOutTimer extends ObjectTimer<h2d.Object> {
 			object.remove();
 		else
 			object.alpha = 0.0;
-
-		final pool = this.pool;
-		if (pool.isSome()) {
-			pool.unwrap().put(this);
-			this.pool = Maybe.none();
-		}
 	}
 }
 
-class FadeOutTimerPool extends SafeObjectPool<FadeOutTimer> {
-	public function new(capacity: UInt) {
-		super(capacity, () -> new FadeOutTimer());
+final class PooledFadeOutTimer extends FadeOutTimer {
+	/**
+		The object pool to which `this` belongs.
+	**/
+	var pool: SafeObjectPool<PooledFadeOutTimer>;
+
+	public function new(pool: SafeObjectPool<PooledFadeOutTimer>) {
+		super();
+		this.pool = pool;
 	}
 
-	override public function get(): FadeOutTimer {
+	override function onComplete(): Void {
+		super.onComplete();
+		this.pool.put(this);
+	}
+}
+
+class FadeOutTimerPool extends SafeObjectPool<PooledFadeOutTimer> {
+	public function new(capacity: UInt) {
+		super(capacity, () -> new PooledFadeOutTimer(this));
+	}
+
+	override public function get(): PooledFadeOutTimer {
 		throw "Not implemented. Call use() instead of get().";
 	}
 
 	/**
-		Returns a `FadeOutTimer` instance that is currently not in use,
+		Returns a `PooledFadeOutTimer` instance that is currently not in use,
 		and also resets some variables. Use this method instead of `get()`.
 
 		The instance is automatically recycled when completed so that it can be reused again
@@ -59,18 +65,18 @@ class FadeOutTimerPool extends SafeObjectPool<FadeOutTimer> {
 		@param object The object to change the alpha value.
 		@param duration
 		@param removeOnComplete If `true`, calls `object.remove()` when completing.
-		@return A `FadeOutTimer` instance.
+		@return A `PooledFadeOutTimer` instance.
 	**/
-	@:access(broker.timer.builtin.heaps.FadeOutTimer)
+	@:access(broker.timer.builtin.heaps.PooledFadeOutTimer)
 	public function use(
 		object: h2d.Object,
 		duration: UInt,
 		removeOnComplete = false
-	): FadeOutTimer {
+	): PooledFadeOutTimer {
 		final timer = super.get();
 		TimerExtension.reset(timer, duration);
 		timer.object = object;
-		final pool: SafeObjectPool<FadeOutTimer> = this;
+		final pool: SafeObjectPool<PooledFadeOutTimer> = this;
 		timer.pool = pool;
 		timer.removeOnComplete = removeOnComplete;
 		return timer;
