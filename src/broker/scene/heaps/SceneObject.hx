7305b703e6bdda083dc287e881511e110fe6e008
FAL <falworks.contact@gmail.com>
2020-06-28 01:28:06 +0900

    scene: add SceneObject.new()

--- src/broker/scene/heaps/SceneObject.hx
+++ src/broker/scene/heaps/SceneObject.hx
@@ -5,5 +5,9 @@ abstract SceneObject(h2d.Object) from h2d.Object to h2d.Object {
 	public static function fromImage(image: hxd.res.Image): SceneObject {
 		return new h2d.Bitmap(image.toTile());
 	}
+
+	public extern inline function new() {
+		this = new h2d.Object();
+	}
 }
 #end
