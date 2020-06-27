313b686100aaea1faaa75b9a5b0d061e0a03565b
FAL <falworks.contact@gmail.com>
2020-06-28 01:28:14 +0900

    scene: refactor Layer

--- src/broker/scene/heaps/Layer.hx
+++ src/broker/scene/heaps/Layer.hx
@@ -1,23 +1,23 @@
 package broker.scene.heaps;
 
 #if heaps
-abstract Layer(h2d.Object) from h2d.Object to h2d.Object {
+abstract Layer(h2d.Object) from h2d.Object to h2d.Object to SceneObject {
 	/**
 		`this` as the underlying type.
 	**/
 	public var data(get, never): h2d.Object;
 
-	public function new() {
+	public extern inline function new() {
 		this = new h2d.Object();
 	}
 
-	public function add(object: SceneObject): Void
+	public extern inline function add(object: SceneObject): Void
 		this.addChild(object);
 
-	public function remove(object: SceneObject): Void
+	public extern inline function remove(object: SceneObject): Void
 		this.removeChild(object);
 
-	public function clear(): Void
+	public extern inline function clear(): Void
 		this.removeChildren();
 
 	extern inline function get_data()
