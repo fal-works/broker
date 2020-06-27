cf7160afbcf59d89c993ed4ef91598ab826b919f
FAL <falworks.contact@gmail.com>
2020-06-28 01:21:47 +0900

    scene: add Layer.new/data

--- src/broker/scene/heaps/Layer.hx
+++ src/broker/scene/heaps/Layer.hx
@@ -2,6 +2,15 @@ package broker.scene.heaps;
 
 #if heaps
 abstract Layer(h2d.Object) from h2d.Object to h2d.Object {
+	/**
+		`this` as the underlying type.
+	**/
+	public var data(get, never): h2d.Object;
+
+	public function new() {
+		this = new h2d.Object();
+	}
+
 	public function add(object: SceneObject): Void
 		this.addChild(object);
 
@@ -10,5 +19,8 @@ abstract Layer(h2d.Object) from h2d.Object to h2d.Object {
 
 	public function clear(): Void
 		this.removeChildren();
+
+	extern inline function get_data()
+		return this;
 }
 #end
