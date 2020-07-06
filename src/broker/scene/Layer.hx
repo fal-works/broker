6f3c0b6786f4b6be20affd21a9a4d868b725e1e1
FAL <falworks.contact@gmail.com>
2020-07-06 18:57:05 +0900

    scene: refactor

--- src/broker/scene/Layer.hx
+++ src/broker/scene/Layer.hx
@@ -3,8 +3,26 @@ package broker.scene;
 /**
 	Drawing layer.
 **/
-#if heaps
-typedef Layer = broker.scene.heaps.Layer;
-#else
-typedef Layer = Dynamic;
-#end
+@:notNull @:forward(setFilter)
+abstract Layer(SceneObject) from SceneObject to SceneObject {
+	/**
+		`this` as the underlying type.
+	**/
+	public var data(get, never): SceneObject;
+
+	public extern inline function new() {
+		this = new SceneObject();
+	}
+
+	public extern inline function add(object: SceneObject): Void
+		this.addChild(object);
+
+	public extern inline function remove(object: SceneObject): Void
+		this.removeChild(object);
+
+	public extern inline function clear(): Void
+		this.removeChildren();
+
+	extern inline function get_data()
+		return this;
+}
--- src/broker/scene/heaps/SceneObject.hx
+++ src/broker/scene/heaps/SceneObject.hx
@@ -3,7 +3,7 @@ package broker.scene.heaps;
 #if heaps
 import broker.scene.internal.SceneObjectData;
 
-@:forward(x, y, setPosition)
+@:forward(x, y, setPosition, addChild, removeChild, removeChildren)
 abstract SceneObject(SceneObjectData) from SceneObjectData to SceneObjectData {
 	/**
 		Creates a `SceneObject` from `image` bitmap.
@@ -20,6 +20,12 @@ abstract SceneObject(SceneObjectData) from SceneObjectData to SceneObjectData {
 	public extern inline function new() {
 		this = new SceneObjectData();
 	}
+
+	/**
+		Sets `filter` to `this` object.
+	**/
+	public extern inline function setFilter(filter: Filter): Void
+		this.filter = filter;
 
 	extern inline function get_data()
 		return this;
