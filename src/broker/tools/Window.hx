8df487883bf97b1e02f19fa8fa250b65930190c8
FAL <falworks.contact@gmail.com>
2020-07-08 15:52:16 +0900

    tools: improve Window.fullscreen

--- src/broker/tools/Window.hx
+++ src/broker/tools/Window.hx
@@ -1,11 +1,17 @@
 package broker.tools;
 
+import sneaker.log.Logger;
 import broker.sound.SoundManager;
 
 /**
 	Static fields related to the window.
 **/
 class Window {
+	/**
+		`true` if the window is currently fullscreen.
+	**/
+	public static var fullscreen(default, set): Bool = false;
+
 	/**
 		Function called when the window is closed.
 	**/
@@ -24,14 +30,11 @@ class Window {
 	}
 
 	/**
-		Sets/unsets fullscreen mode.
-		@param flag `true` for fullscreen, `false` for windowed.
+		Toggles the value of `fullscreen`.
+		@return The value of `fullscreen` after toggled.
 	**/
-	public static function fullscreen(flag: Bool): Void {
-		#if heaps
-		hxd.Window.getInstance().displayMode = if (flag) Fullscreen else Windowed;
-		#end
-	}
+	public static function toggleFullscreen(): Bool
+		return fullscreen = !fullscreen;
 
 	/**
 		Closes the window.
@@ -49,5 +52,21 @@ class Window {
 	static function prepareClose(): Void {
 		SoundManager.disposeAll();
 		onClose();
+	}
+
+	static extern inline function set_fullscreen(flag: Bool): Bool {
+		#if heaps
+		final window = hxd.Window.getInstance();
+		if (flag) {
+			window.displayMode = Fullscreen;
+		} else {
+			window.displayMode = Windowed;
+			window.resize(App.width, App.height);
+		}
+		#end
+
+		Logger.debug('Set fullscreen: $flag');
+
+		return fullscreen = flag;
 	}
 }
