f6e84db15a8d3dedeac0b8833d7350bc04b0ac1c
FAL <falworks.contact@gmail.com>
2020-07-09 08:49:10 +0900

    image: add Tile.fromColor()

--- src/broker/image/heaps/Tile.hx
+++ src/broker/image/heaps/Tile.hx
@@ -1,9 +1,21 @@
 package broker.image.heaps;
 
+import broker.color.ArgbColor;
+
 @:notNull
 abstract Tile(h2d.Tile) from h2d.Tile to h2d.Tile {
+	/**
+		Creates a new `Tile` instance from `image`.
+		Available only `#if heaps`.
+	**/
 	public static extern inline function fromImage(image: hxd.res.Image): Tile
 		return image.toTile();
+
+	/**
+		Creates a new `Tile` instance from `color`.
+	**/
+	public static extern inline function fromColor(color: ArgbColor, width: UInt, height: UInt): Tile
+		return h2d.Tile.fromColor(color.getRGB().int(), width, height, color.getAlpha().float());
 
 	public var width(get, never): UInt;
 	public var height(get, never): UInt;
